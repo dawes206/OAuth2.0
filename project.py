96cb7b7b5c1282d303003547394d5b6def47dd60
Silas, Walker (sw099y) <sw099y@att.com>
2019-06-27 17:29:43 -0700

    added client_secrets file and imports for next step

--- project.py
+++ project.py
@@ -9,6 +9,9 @@ from database_setup import Base, Restaurant, MenuItem
 from flask import session as login_session
 import random, string
 
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
